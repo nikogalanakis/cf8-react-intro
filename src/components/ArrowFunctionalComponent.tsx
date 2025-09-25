2f83df7bc3658df6ff18a21b9d03b5f61d4216d4
NGALANAKIS <galanakis.delfi@aueb.gr>
2025-09-25 20:55:07 +0300

    feat: examples of Component Usage

---
+++ src/components/ArrowFunctionalComponent.tsx
@@ -0,0 +1,6 @@
+const ArrowFunctionalComponent = () => {
+    const title = "Is an ArrowFunctionalComponent";
+    return <h1 className="text-center text-xl font-bold mt-12">{title}</h1>
+}
+
+export default ArrowFunctionalComponent;
